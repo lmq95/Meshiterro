329239ef32cbbb8becaf325f593578b63addd67a
lmq95 <4321manap@gmail.com>
2021-10-14 14:37:02 +0000

    [finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/show'
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
